6179a7f70cca260fc5b1e4c76d80ce53f3d5fbfd
Dearvip0 <baothuong9608@gmail.com>
2023-10-04 10:51:48 +0700

    add header with router and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { About, Home, Profile, SignIn, SignUp } from "./pages";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
